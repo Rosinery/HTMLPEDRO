a4f700de7692e33f77d425dad59a93fc79144ffd
Rosinery <92558971+Rosinery@users.noreply.github.com>
2022-12-01 16:06:56 -0300

    Add files via upload

---
+++ kwitterRoom.js
@@ -0,0 +1,16 @@
+
+//ADICIONE SEUS LINKS FIREBASE
+
+
+function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+       roomNames = childKey;
+       console.log("Nome da Sala - " + roomNames);
+      row = "<div class='roomName' id="+roomNames+" onclick='redirectToRoomName(this.id)' >#"+ roomNames +"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+    });
+  });
+
+}
+
+getData();
+
